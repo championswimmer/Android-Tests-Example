cf104c6a1849c0ad587c61f3a0fb4c0a00b26e05
Arnav Gupta <championswimmer@gmail.com>
2016-01-06 18:09:34 +0530

    step 2 : add km to fare conversion

--- app/src/main/java/in/championswimmer/android_tests_example/MainActivity.java
+++ app/src/main/java/in/championswimmer/android_tests_example/MainActivity.java
@@ -2,12 +2,47 @@ package in.championswimmer.android_tests_example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText km;
+    Button calc;
+    TextView fare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        km = (EditText) findViewById(R.id.editText);
+        calc = (Button) findViewById(R.id.button);
+        fare = (TextView) findViewById(R.id.textView);
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double dist = Double.valueOf(km.getText().toString());
+                String rupees = formatFare(fareFromKm(dist));
+                fare.setText(rupees);
+
+            }
+        });
+
+    }
+
+    public static String formatFare (double fare) {
+        DecimalFormat precision = new DecimalFormat("0.00");
+        return precision.format(fare);
+    }
+
+    public static Double fareFromKm (double km) {
+        double fare = (25) + ((km - 2) * 9);
+        return fare;
     }
 }
