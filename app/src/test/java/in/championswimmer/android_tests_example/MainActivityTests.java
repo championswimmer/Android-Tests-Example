4026467e82aa163bcfc6d84853308d7b18a42038
Arnav Gupta <championswimmer@gmail.com>
2016-01-19 03:49:10 +0530

    step 3: Add unit test for fare calc method
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ app/src/test/java/in/championswimmer/android_tests_example/MainActivityTests.java
@@ -0,0 +1,18 @@
+package in.championswimmer.android_tests_example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by championswimmer on 6/1/16.
+ */
+public class MainActivityTests {
+
+    @Test
+    public void testFareFromKm() throws Exception {
+        assertEquals(25.0, MainActivity.fareFromKm(2), 0.001);
+        assertEquals(34.0, MainActivity.fareFromKm(3), 0.001);
+    }
+}
