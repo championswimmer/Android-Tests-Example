7d88a2c634764ffc878c541f1d73ff1b0b20ab8d
Arnav Gupta <championswimmer@gmail.com>
2016-03-20 13:38:36 +0530

    step 5: Add instrumentation setup and skeleton

--- app/src/androidTest/java/in/championswimmer/android_tests_example/MainActivityTest.java
+++ app/src/androidTest/java/in/championswimmer/android_tests_example/MainActivityTest.java
@@ -3,33 +3,37 @@ package in.championswimmer.android_tests_example;
 import android.app.Application;
 import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.InstrumentationTestCase;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 /**
  * Created by championswimmer on 20/3/16.
  */
-
 @RunWith(AndroidJUnit4.class)
-public class ApplicationTest {
+public class MainActivityTest {
 
-    private Application app;
-    public final InstrumentationTestCase instRunner = new InstrumentationTestCase();
+    private MainActivity mainActivity;
+
+    @Rule
+    public final ActivityTestRule<MainActivity> act = new ActivityTestRule<MainActivity>(MainActivity.class);
+
 
     @Before
     public void setUp() throws Exception {
+        mainActivity = act.getActivity();
 
-        instRunner.injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        app = Instrumentation.newApplication(Application.class, instRunner.getInstrumentation().getTargetContext());
     }
 
     @After
     public void tearDown() throws Exception {
+        mainActivity.finish();
 
     }
+
 
 }
